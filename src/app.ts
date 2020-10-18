7e919c29a8b787478b19f516df0ee9c121f40d2d
Nicolas Juanico <nicolas.juany@gmail.com>
2020-10-18 16:14:47 -0300

    Rename element > form, get inputs and handle submit

--- src/app.ts
+++ src/app.ts
@@ -3,20 +3,38 @@
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
-  element: HTMLFormElement;
+  form: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
     this.hostElement = document.getElementById('app')! as HTMLDivElement;
 
     const importedNode = document.importNode(this.templateElement.content, true);
-    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.form = importedNode.firstElementChild as HTMLFormElement;
+    this.form.id = 'user-input';
+
+    this.titleInputElement = this.form.querySelector('#title')! as HTMLInputElement;
+    this.descriptionInputElement = this.form.querySelector('#description')! as HTMLInputElement;
+    this.peopleInputElement = this.form.querySelector('#people')! as HTMLInputElement;
 
     this.attach();
+    this.configure();
+  }
+
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(event);
+  }
+
+  private configure() {
+    this.form.addEventListener('submit', this.submitHandler);
   }
 
   private attach() {
-    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+    this.hostElement.insertAdjacentElement('afterbegin', this.form);
   }
 }
 
