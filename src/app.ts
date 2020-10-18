a1398fa1d99ac277927a7f4cf73b4433953605f1
Nicolas Juanico <nicolas.juany@gmail.com>
2020-10-18 16:53:17 -0300

    gather user inputs, validate and return a tuple of values, clean inputs after

--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,8 @@ function Autobind(
   return modified;
 }
 
+type gatherUser = [string, string, number] | void;
+
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
@@ -40,12 +42,37 @@ class ProjectInput {
     this.attach();
   }
 
+  private gatherUserInput(): gatherUser {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    if (
+      !enteredTitle.trim().length ||
+      !enteredDescription.trim().length ||
+      !enteredPeople.trim().length
+    ) {
+      return;
+    } else {
+      [this.titleInputElement, this.descriptionInputElement, this.peopleInputElement].forEach((x) =>
+        this.clearInput(x)
+      );
+      return [enteredTitle, enteredDescription, +enteredPeople];
+    }
+  }
+
   @Autobind
   private submitHandler(event: Event) {
     event.preventDefault();
-    console.log(this.titleInputElement.value);
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      console.log(userInput);
+    }
   }
-  
+
+  private clearInput(input: HTMLInputElement) {
+    input.value = '';
+  }
 
   private configure() {
     this.form.addEventListener('submit', this.submitHandler);
