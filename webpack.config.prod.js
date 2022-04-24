272fd61815a715d3526f1088eb8be0bc35403cb9
Nicolas Juanico <nicolas.juany@gmail.com>
2022-04-24 13:14:10 +0200

    Fix webpack

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,30 +1,28 @@
-const path = require('path');
-const CleanPlugin = require('clean-webpack-plugin')
+const path = require("path");
+const CleanPlugin = require("clean-webpack-plugin");
 
 module.exports = {
   context: __dirname,
-  mode: 'production',
-  entry: './src/app.ts',
+  mode: "production",
+  entry: "./src/app.ts",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
-  devtool: 'none',
+  devtool: "inline-source-map",
   module: {
     rules: [
       {
         test: /\.ts$/,
         exclude: /node_modules/,
         use: {
-          loader: 'ts-loader',
+          loader: "ts-loader",
         },
       },
     ],
   },
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: [".ts", ".js"],
   },
-  plugins: [
-    new CleanPlugin.CleanWebpackPlugin()
-  ]
+  plugins: [new CleanPlugin.CleanWebpackPlugin()],
 };
